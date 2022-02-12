4e6a181fd50309bebd0bec8f9b6c6c4fb7fe59ff
mrming112 <suhaiming41@gmail.com>
2022-02-12 23:32:02 +0700

    first commit

---
+++ pages/about/index.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <div class="container">
+            <h2>About ComSci SKRU</h2>
+            <h1>Computer Science</h1>
+            <h3>Songkhla Rajabhat University</h3>
+            <img src="./SKRU.jpg" >
+            <h3>หลักสูตรวิทการคอมพิวเตอร์ มหาวิทยาลัยราชภัฎสงขลา</h3>
+            <h4>"เป็นหลักสูตรที่ปั้นคนเก่งมากมาย และ หลากหลาย สร้างคนให้เป็นคนที่กล้าแสดงออกและแสดงศักยภาพของตนเองออกมา"</h4>
+        </div>
+    </div>
+</template>
+<style scoped>
+img{
+    margin: 30px;
+    height: 250px;
+}
+.container{
+    text-align: center;
+}
+h2{
+    background-color: black;
+    color: white;
+    padding: 10px;
+}
+h1{
+    margin: 10px;
+}
+h3{
+    color: gray;
+    margin: 10px;
+}
+h4{
+    color: rgb(255, 196, 0);
+}
+</style>
